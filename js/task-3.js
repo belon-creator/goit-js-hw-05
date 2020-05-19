3b6b1390142973cb679eae4b96eae94c83dc7fb1
belon-creator <belon8911@gmail.com>
2020-05-19 18:28:44 +0300

    add all tasks

---
+++ js/task-3.js
@@ -0,0 +1,38 @@
+'use strict';
+
+console.log('=====================task-3==================');
+
+class Storage {
+  constructor(products) {
+    this.items = products;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    let idx = this.items.indexOf(item);
+    this.items.splice(idx, 1);
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
